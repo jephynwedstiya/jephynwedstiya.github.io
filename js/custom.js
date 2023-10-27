5a00e674177d7ba11cacd6093ba5ab8750bc0916
jobinkv <jobin.kv@research.iiit.ac.in>
2023-10-27 14:26:43 +0530

    eror update

--- js/custom.js
+++ js/custom.js
@@ -86,7 +86,8 @@
 			image: {
 				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
 				titleSrc: function(item) {
-					return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+					// return item.el.attr('title') + '<small> </small>';
+					return 'Jephyn Weds Tiya' + '<small> </small>';
 				}
 			}
 		});
